81a23389e5c99d19a11c3b197fb03cfbcc705036
phillipug <phillipmusiime@gmail.com>
2020-05-16 18:17:44 +0300

    Corrected rubocop errors

--- lib/movie_scraper.rb
+++ lib/movie_scraper.rb
@@ -1,71 +1,75 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'open-uri'
 require 'byebug'
 
-
-Url1 = "https://o2tvseries.com/search/list_all_tv_series"
-Url2 = "https://toxicwap.com/New_Movies/az/"
-
+Url1 = 'https://o2tvseries.com/search/list_all_tv_series'
+Url2 = 'https://toxicwap.com/New_Movies/az/'
 
 # MOVIES
 
 def page(letter)
-		puts "Loading Movies in category [#{letter}]...."
-		page_url = Url2 + letter + '.php'
-		html = Nokogiri::HTML(URI.open(page_url))
-		html.css('li').map {|title| title.text}
+  puts "Loading Movies in category [#{letter}]...."
+  page_url = Url2 + letter + '.php'
+  html = Nokogiri::HTML(URI.open(page_url))
+  html.css('li').map(&:text)
 end
 
 def all_movies
-	arr= []
-('a'..'z').each {|letter| arr << page(letter) unless letter == 'x'}
-	arr.flatten
+  arr = []
+  ('a'..'z').each { |letter| arr << page(letter) unless letter == 'x' }
+  arr.flatten
 end
 
 def movies_by_year(year)
-	puts "Fetching movies ...."
-	puts ""
-	movies = all_movies
+  puts 'Fetching movies ....'
+  puts ''
+  movies = all_movies
 
-	movies.keep_if do |title|
-		sanitized = title.scan(/\d{4}/).join
-		title if sanitized.to_i == year
-	end
+  movies.keep_if do |title|
+    sanitized = title.scan(/\d{4}/).join
+    title if sanitized.to_i == year
+  end
 end
 
 def movies_by_title(title)
-	movies = all_movies
+  movies = all_movies
 
-	results = []
-	movies.each {|item| results << item if item.downcase.include?(title.downcase)}
-	results.empty? ? ["Movie Not Found!"] : results 
+  results = []
+  movies.each do |item|
+    results << item if item.downcase.include?(title.downcase)
+  end
+  results.empty? ? ['Movie Not Found!'] : results.sort
 end
 
 # SERIES
 
 def all_series
-	puts
-	puts "Loading..."
-	html = Nokogiri::HTML(URI.open(Url1))
-	html.css('.data a').map {|title| title.text}
+  puts
+  puts 'Loading...'
+  html = Nokogiri::HTML(URI.open(Url1))
+  html.css('.data a').map(&:text).sort
 end
 
 def serie_by_title(title)
-	puts
-	puts "Loading series that match '#{title}'..."
-	puts 
-	series = all_series
+  puts
+  puts "Loading series that match '#{title}'..."
+  puts
+  series = all_series
 
-	results = []
-	series.each {|item| results << item if item.downcase.include?(title.downcase)}
-	results.empty? ? ["Movie Not Found!"] : results 
+  results = []
+  series.each do |item|
+    results << item if item.downcase.include?(title.downcase)
+  end
+  results.empty? ? ['Movie Not Found!'] : results.sort
 end
 
 def latest
-	puts
-	puts "Loading newest series..."
-	puts '-------------------------'
-	url = Url1[0...-18] + 'recently_added'
-	html = Nokogiri::HTML(URI.open(url))
-	html.css('.data').map {|title| title.text.gsub(/[\t\n]/, '').gsub('  ', ' ').gsub('Season', 'Sn.').gsub('Episode', 'Ep.').insert(-2, "/#{Time.now.year}")}
+  puts
+  puts 'Loading newest series...'
+  puts '-------------------------'
+  url = Url1[0...-18] + 'recently_added'
+  html = Nokogiri::HTML(URI.open(url))
+  html.css('.data').map { |title| title.text.gsub(/[\t\n]/, '').gsub('  ', ' ').gsub('Season', 'Sn.').gsub('Episode', 'Ep.').insert(-2, "/#{Time.now.year}") }
 end
